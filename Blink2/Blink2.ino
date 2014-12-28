00fd4fc66070cf8ade535d99b2d32c9d63944e26
Zsolt Cserna <cserna.zsolt@gmail.com>
2014-12-28 14:01:06 +0100

    Blink2.ino: added pushbutton handling

--- Blink2/Blink2.ino
+++ Blink2/Blink2.ino
@@ -1,16 +1,18 @@
 
+#define DELAY 200
+#define MIN_DELAY 50
+#define PIN_IN 2
+
 int leds[] = {3, 5, 6, 9, 10};
 int no_leds = 5;
-
 
 void setup() {
     int i;
     for (i=0; i<no_leds;i++) {
         pinMode(leds[i], OUTPUT);
     }
+    pinMode(PIN_IN, INPUT);
 }
-
-#define DELAY 200
 
 int oldpin = -1;
 
@@ -25,15 +27,23 @@ void moveto(int pin) {
 void loop() {
     int i;
     int d;
-    d = DELAY;
-
     for (i=0; i<no_leds;i++) {
         moveto(leds[i]);
+        if (digitalRead(PIN_IN) == HIGH) {
+            d = MIN_DELAY;
+        } else {
+            d = DELAY;
+        }
         delay(d);
     }
 
     for (i=no_leds-2; i>0;i--) {
         moveto(leds[i]);
+        if (digitalRead(PIN_IN) == HIGH) {
+            d = MIN_DELAY;
+        } else {
+            d = DELAY;
+        }
         delay(d);
     }
 }
